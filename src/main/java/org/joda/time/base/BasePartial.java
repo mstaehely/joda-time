27e3cabb4fa84ee802d1e972ce09f8b47d15ee46
mstaehely <mstaehely@gmail.com>
2017-03-09 15:32:05 -0800

    Update BasePartial.java

--- src/main/java/org/joda/time/base/BasePartial.java
+++ src/main/java/org/joda/time/base/BasePartial.java
@@ -285,7 +285,7 @@ public abstract class BasePartial
      * @throws IndexOutOfBoundsException if the index is invalid
      */
     @SuppressWarnings("index:argument.type.incompatible")
-    // Array length guarantees
+    // Variable array lengths
     // Unable to annotate values as having the same length as iValues.i
     // DateTimeField does not guarantee the length of its arrays.
     protected void setValue(@IndexFor("iValues") int index, int value) {
@@ -304,7 +304,7 @@ public abstract class BasePartial
      * @param values  the array of values
      */
     @SuppressWarnings("index:argument.type.incompatible")
-    // Array length guarantees
+    // Variable array lengths
     // No way to guarantee that values has the same length as iValues
     // SameLen does not satisfy the checker.
     protected void setValues(int[] values) {
